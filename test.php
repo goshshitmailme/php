b6e7d0808a237a02025cc5438a98907587b7595a
gosh <gosh@shitmail.me>
2016-01-14 18:23:56 +0200

    data types

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = 2.3;
+$b = 3;
+$text = "sdfgfdsd";
+$bool = true; // Comment
+
+//array
+//object
+//resource
+
+
+//echo $bool;
+
+$int = (float)"43.54";
+
+echo $int;
+
+var_dump($text);
