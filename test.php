5bba9ec5e100923b2d46055481c0c9c811fb3d51
gosh <gosh@shitmail.me>
2016-01-18 18:35:35 +0200

    Less 2

--- test.php
+++ test.php
@@ -1,19 +1,75 @@
 <?php
-
+// psr-2
 $a = 2.3;
 $b = 3;
 $text = "sdfgfdsd";
 $bool = true; // Comment
+$int = (float)"43.54";
 
 //array
+/*$arr = array ();
+$arr = [
+    "one" => 1,
+    "two" => 2,
+    3,
+];
+$arr = [
+    4 => 1,
+    2,
+    3,
+];*/
+
 //object
 //resource
 
 
 //echo $bool;
 
-$int = (float)"43.54";
 
-echo $int;
+//echo $int;
 
 //var_dump($text);
+//var_dump($arr);
+//echo $arr["one"];
+
+$serverLangs = [
+    "php",
+    "python",
+    "ruby",
+];
+$clientLangs = [
+    "JavaScript",
+];
+
+$langs = [
+    "server" => $serverLangs,
+    "client" => $clientLangs
+];
+
+//var_dump($langs);
+//print $langs[0][2];
+$c = count($langs["server"]);
+for ($i = 0, $c = count($langs["server"]); $i < $c; $i++) {
+//    print $langs["server"][$i];
+
+}
+
+foreach ($langs as $key => $lang) {
+    for ($i = 0, $c = count($langs[$key]); $i < $c; $i++) {
+//        print $lang[$i];
+    }
+}
+
+print "<ul>" . "\n";
+foreach ($langs as $langGroup) {
+    foreach ($langGroup as $lang) {
+//        print "<li>";
+//        print $lang;
+//        print "</li>";
+//        print "<li>" . $lang . "</li>";
+        print "\t<li>{$lang}1</li>\n";
+//        print '<li>$lang</li>';
+
+    }
+}
+print "</ul>";
